b53ae5c3f34c6c20b8cdce581d29d3f05d12b0ca
Andrea <andrea@yaaaw.it>
2022-05-26 10:53:23 +0200

    refactor(download.py): miss binary mode for outFile

--- cli/data/enti/download.py
+++ cli/data/enti/download.py
@@ -36,7 +36,7 @@ def computeOutDir(argv):
         )
 
     if not os.path.isfile(os.path.join(dirName, "README.md")):
-        with open(os.path.join(dirName, "README.md"), 'w') as readmeFile:
+        with open(os.path.join(dirName, "README.md"), "w") as readmeFile:
             readmeFile.write(
                 f"""
                 This folder has been created by MonitoraPA on {os.path.basename(dirName)}.
@@ -70,13 +70,13 @@ def computeOutDir(argv):
 
 def main():
     outDir = computeOutDir(sys.argv)
-    
+
     url = 'https://indicepa.gov.it/ipa-dati/datastore/dump/d09adf99-dc10-4349-8c53-27b1e5aa97b6?bom=True&format=tsv'
     response = requests.get(url, allow_redirects=True)
     result = response.content.replace(b'HTTPS:SISTEMAAMBIENTELUCCA.IT', b'HTTPS://SISTEMAAMBIENTELUCCA.IT')
 
-    with open(f"{outDir}/enti.tsv", "w") as outFile:
-        outFile.write(str(result))
+    with open(f"{outDir}/enti.tsv", "wb") as outFile:
+        outFile.write(result)
 
     print(f"\x1b[1m[ ✅ ] Done. You can find the dataset at \x1b[36m{outDir}/enti.tsv\x1b[39m directory\x1b[0m")
 
