052cefa67efe4af138a1b6e862f7bc75e37edf2b
Giacomo Tesio <giacomo@tesio.it>
2022-06-21 12:36:34 +0200

    check/google_analytics.js: better handling of external JS and spaces

--- check/google_analytics.js
+++ check/google_analytics.js
@@ -25,31 +25,31 @@ setTimeout(function(){
     }
     var html = document.all[0].innerHTML;
     html = html.replaceAll(/\n/g,' ').replaceAll('ga-disable-UA-', '').replaceAll(/<!--[\s\S]*?-->/g, ''); // cleanup comments in html
-    var test = html.match(/ga\(['"]create['"], ?['"]([^'"]*)['"]/);
+    var test = html.match(/ga\(['"]create['"],\s*['"]([^'"]*)['"]/);
     if(test){
-        console.log(`found in html.match(/ga\(['"]create['"], ?['"]([^'"]*)['"]/)`, test);
+        console.log(`found in html.match(/ga\(['"]create['"],\s*['"]([^'"]*)['"]/)`, test);
     } else {
-        var test = html.match(/ga\('create', ?{[^}]*}/gm);
+        var test = html.match(/ga\('create',\s*{[^}]*}/gm);
         if(test){
             objStr = test[0];
             objStr = objStr.replace ("ga('create',", 'window.MonitoraPAObj = ');
             objStr = objStr.replace ('ga("create",', 'window.MonitoraPAObj = ');
             eval(objStr);
             test[1] = window.MonitoraPAObj.trackingId;
-            console.log(`found in html.match(/ga\('create', ?{[^}]*}/gm);`, window.MonitoraPAObj);
+            console.log(`found in html.match(/ga\('create',\s*{[^}]*}/gm);`, window.MonitoraPAObj);
         }
     }
     if(!test){
-        test = html.match(/gtag\(['"]config['"], ?['"]([^'"]*)['"]/);
+        test = html.match(/gtag\(['"]config['"],\s*['"]([^'"]*)['"]/);
         if(test && test[1].substr(0,3) != "UA-" && test[1].substr(0,2) != "G-"){
             test = null;
         }
         if(test){
-            console.log(`found in html.match(/gtag\(['"]config['"], ?['"]([^'"]*)['"]/)`, test);
+            console.log(`found in html.match(/gtag\(['"]config['"],\s*['"]([^'"]*)['"]/)`, test);
         }
     }
     if(!test){
-        test = html.match(/push\(\[['"]_setAccount['"], ?['"]([^'"]*)['"]\]/);
+        test = html.match(/push\(\s*\[\s*['"]_setAccount['"]\s*,\s*['"]([^'"]*)['"]\s*\]/);
         if(test && test[1].substr(0,3) != "UA-" && test[1].substr(0,2) != "G-"){
             test = null;
         }
@@ -59,32 +59,34 @@ setTimeout(function(){
     }
     if(!test || test[1].match(/_ID/)){
         for(var sc of document.getElementsByTagName('script'))
-            if(sc.src.indexOf('googletagmanager') > -1) {
+            if(!test && sc.src.indexOf('googletagmanager') > -1) {
                 test = sc.src.match(/UA-[^&]+/);
                 if(test){
                     console.log(`found in '${sc.src}'`, test);
                     test[1] = test[0]
-                } else {
-                    var srcURI = sc.src;
-                    var txtFile = new XMLHttpRequest();
-                    txtFile.open("GET", srcURI, true);
-                    txtFile.onreadystatechange = function(){  
-                        if (txtFile.readyState === 4) {
-                            var content = txtFile.responseText;
-                            var tId = content.match(/UA-[^'"]+/);
-                            if(tId){
-                                if(tId.indexOf('d') != -1){
-                                    document.title = "GA PRESENT BUT MISCONFIGURED";
-                                    console.log(`found inside '${srcURI}'`, document.title);
-                                } else {
-                                    document.title = tId[0];
-                                    console.log(`found inside '${srcURI}'`, tId);
-                                }
-                            }
-                        } 
-                    }
-                    txtFile.send();
                 }
+            }
+    }
+    if(!test || test[1].match(/_ID/)){
+        for(var sc of document.getElementsByTagName('script'))
+            if(sc.src.indexOf('googletagmanager') > -1) {
+                
+                var srcURI = sc.src;
+                var txtFile = new XMLHttpRequest();
+                txtFile.open("GET", srcURI, true);
+                txtFile.onreadystatechange = function(){  
+                    if (txtFile.readyState === 4) {
+                        var content = txtFile.responseText;
+                        var tId = content.match(/UA-[^'"]+/);
+                        if(tId){
+                            if(tId[0].indexOf('d') != -1){
+                                document.title = tId[0];
+                                console.log(`found inside '${srcURI}'`, tId);
+                            }
+                        }
+                    } 
+                }
+                txtFile.send();
             }
     }
 
